bd2510a75549635f147de1a5207b06f356e3c478
pandit ji <pandit ji@DESKTOP-37UDTFK>
2022-04-01 10:51:39 +0530

    Add project files.

---
+++ MVCWebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWebApplication4.Models;
+
+namespace MVCWebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //public ActionResult ContactUs()
+        //{
+        //    Provider p = new Provider();
+        //    //p.ProviderId = 12;
+        //    //p.ProviderName = "Vikas";
+        //    //p.ProviderType = "Claim";
+        //    //ViewBag.Message = "Welcome to the Tata Group";
+
+        //    var data = p.GetProviderData();
+
+        //    p.ProviderId = Convert.ToInt32(data.Tables[0].Rows[0][0].ToString());
+        //    p.ProviderName = data.Tables[0].Rows[0][1].ToString();
+        //    p.ProviderType = data.Tables[0].Rows[0][2].ToString();
+        //    return View(p);
+        //}
+
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(new Provider());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Provider p)
+        {
+            if (ModelState.IsValid)
+            {
+                Provider newP = p;
+                p.InsertProvider(p);
+            }
+
+            return RedirectToAction("ShowProvider");
+        }
+
+        public ActionResult ShowProvider()
+        {
+            List<Provider> p = new List<Provider>();
+
+            var dataTables = Provider.GetProviderData();
+            
+            foreach (DataRow data in dataTables.Tables[0].Rows)
+            {
+                //Provider newProvider = new Provider()
+                //{
+                //    ProviderId = Convert.ToInt32(data[0].ToString()),
+                //    ProviderName = data[1].ToString(),
+                //    ProviderType = data[2].ToString()
+                //};
+
+                //p.Add(newProvider);
+
+                p.Add(new Provider()
+                {
+                    ProviderId = Convert.ToInt32(data[0].ToString()),
+                    ProviderName = data[1].ToString(),
+                    ProviderType = data[2].ToString()
+                });
+            }
+            return View(p);    
+        }
+
+    }
+}
---
+++ MVCWebApplication4/Models/Provider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCWebApplication4.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderType { get; set; }
+
+
+        public static DataSet GetProviderData()
+        {
+            
+            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+
+            conn.Open();
+            SqlCommand command = new SqlCommand("select * from Provider", conn);
+            SqlDataAdapter adaptor = new SqlDataAdapter(command);
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds);
+            return ds;
+        }
+
+        internal void InsertProvider(Provider p)
+        {
+            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string insertCmd = $"insert into Provider (ProviderName, ProviderType) values ('{p.ProviderName}','{p.ProviderType}'";
+            conn.Open();
+            SqlCommand command = new SqlCommand(insertCmd, conn);
+            command.ExecuteNonQuery();
+        }
+    }  
+}
